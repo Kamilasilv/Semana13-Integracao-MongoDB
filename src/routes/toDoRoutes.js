4087bb77413e6b6579f6074900d0752282e961ee
Kamila Silva <kamillasilva140@gmail.com>
2021-08-05 23:25:06 -0300

    update rotas

--- src/routes/toDoRoutes.js
+++ src/routes/toDoRoutes.js
@@ -3,15 +3,14 @@ const router = express.Router()
 
 const controller = require("../controllers/toDoController")
 
-router.get("/", controller.getAllTarefas)
+router.get("/", controller.getAllTarefas) 
 
-router.post("/create", controller.createTask)
+router.post("/create", controller.createTask) 
 
-router.delete("/delete/:id", controller.deleteTask)
+router.delete("/delete/:id", controller.deleteTask) 
 
-router.put("/replace/:id", controller.replaceTask)
+router.put("/replace/:id", controller.replaceTask) 
 
-router.patch("/updateTitle/:id", controller.updateTitle)
-// router.patch("/updated/:id", controller.updateAnyThing)
+router.patch("/updateTitle/:id", controller.updateTitle) 
 
 module.exports = router
